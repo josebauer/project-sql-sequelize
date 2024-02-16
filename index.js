eff5437df7d7661e959b074ffe88a679dd0cabfa
José Bauer <jose.h.bauer03@gmail.com>
2024-02-16 15:29:22 -0300

    Insere dados no DB via Sequelize

---
+++ index.js
@@ -0,0 +1,11 @@
+(async () => {
+  const Planet = require('./models/Planet')
+  
+  const newPlanet = await Planet.create({
+    name: "Venus",
+    position: 5,
+  })
+
+  console.log(newPlanet)
+
+})();
