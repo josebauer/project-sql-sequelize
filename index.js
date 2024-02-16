d3d7eb3cedfe651183a5c9d92de21a662d91b04e
José Bauer <jose.h.bauer03@gmail.com>
2024-02-16 15:47:40 -0300

    Atualiza dados do DB via Sequelize

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 (async () => {
   const Planet = require('./models/Planet')
   
+  // Cria dados
+
   // const newPlanet = await Planet.create({
   //   name: "Venus",
   //   position: 5,
   // })
 
-  const seePlanets = await Planet.findAll({
-    where: {
-      name:"Terra"
-    }
-  })
+  // Consulta dados
 
-  console.log(seePlanets)
+  // const seePlanets = await Planet.findAll({
+  //   where: {
+  //     name:"Terra"
+  //   }
+  // })
+
+  // Atualiza dados
+  
+  const updatePlanets = await Planet.findByPk(2)
+  
+  updatePlanets.name = 'Vênus'
+
+  await updatePlanets.save()
+
+  console.log(updatePlanets)
 
 })();
