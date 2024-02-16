65a825b7d20973d07305c54aeee7d5f58ced88da
José Bauer <jose.h.bauer03@gmail.com>
2024-02-16 15:36:46 -0300

    Consulta dados do DB via Sequelize

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 (async () => {
   const Planet = require('./models/Planet')
   
-  const newPlanet = await Planet.create({
-    name: "Venus",
-    position: 5,
+  // const newPlanet = await Planet.create({
+  //   name: "Venus",
+  //   position: 5,
+  // })
+
+  const seePlanets = await Planet.findAll({
+    where: {
+      name:"Terra"
+    }
   })
 
-  console.log(newPlanet)
+  console.log(seePlanets)
 
 })();
