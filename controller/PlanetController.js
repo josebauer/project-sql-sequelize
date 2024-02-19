da2de00339582e67bb1f24e0e3fa255abc42a264
José Bauer <jose.h.bauer03@gmail.com>
2024-02-19 15:52:00 -0300

    Adiciona método get nas routes e configura PlanetController

--- controller/PlanetController.js
+++ controller/PlanetController.js
@@ -6,5 +6,10 @@ module.exports = {
 
     const planet = await Planet.create({name, position})
     return res.json(planet)
+  },
+
+  async index(req, res) {
+    const planets = await Planet.findAll()
+    return res.json(planets)
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const PlanetController = require("../controller/PlanetController")
 
 routes.post('/planets', PlanetController.store)
+routes.get('/planets', PlanetController.index)
 
 module.exports = routes
