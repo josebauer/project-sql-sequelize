1bf6be0afd8eaf484810d863cc170f4fc95e6aab
José Bauer <jose.h.bauer03@gmail.com>
2024-02-19 17:11:32 -0300

    Adiciona método put e delete nas routes e atualiza o PlanetController

--- controller/PlanetController.js
+++ controller/PlanetController.js
@@ -4,12 +4,34 @@ module.exports = {
   async store(req, res) {
     const { name, position } = req.body
 
-    const planet = await Planet.create({name, position})
+    const planet = await Planet.create({ name, position })
     return res.json(planet)
   },
 
   async index(req, res) {
     const planets = await Planet.findAll()
     return res.json(planets)
+  },
+
+  async put(req, res) {
+    const { name, size, position } = req.body
+    await Planet.update(
+      { name, size, position },
+      {
+        where: {
+          id: req.params.id,
+        }
+      }
+    )
+    return res.send('Planeta atualizado com sucesso!')
+  },
+
+  async delete(req, res) {
+    await Planet.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    return res.send('Planeta deletado com sucesso!')
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const routes = express.Router()
 
 const PlanetController = require("../controller/PlanetController")
 
+// Routes 
 routes.post('/planets', PlanetController.store)
 routes.get('/planets', PlanetController.index)
+routes.put('/planets/:id', PlanetController.put)
+routes.delete('/planets/:id', PlanetController.delete)
 
 module.exports = routes
