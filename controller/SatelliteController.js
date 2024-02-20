337f831b954e5307a7c21e881d4a6d819f9b77a7
José Bauer <jose.h.bauer03@gmail.com>
2024-02-20 17:46:34 -0300

    Cria SatelliteController e adiciona novas rotas

---
+++ controller/SatelliteController.js
@@ -0,0 +1,32 @@
+const Planet = require('../models/Planet')
+const Satellite = require('../models/Satellite')
+
+module.exports = {
+  async store(req, res) {
+    const { planetId } = req.params
+    const { name, serial_number } = req.body
+
+    const planet = await Planet.findByPk(planetId)
+
+    if (!planet) {
+      res.send('Esse planeta não existe!')
+    }
+
+    const satellite = await Satellite.create({ name, serial_number, planetId })
+    return res.json(satellite)
+  },
+
+  async index(req, res) {
+    const { planetId } = await req.params
+
+    if (!planetId) {
+      res.send('Esse planeta não existe!')
+    }
+
+    const planet = await Planet.findByPk(planetId, {
+      include: Satellite
+    })
+
+    return res.json(planet)
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ const express = require("express")
 const routes = express.Router()
 
 const PlanetController = require("../controller/PlanetController")
+const SatelliteController = require("../controller/SatelliteController")
 
-// Routes 
+// Routes planets
 routes.post('/planets', PlanetController.store)
 routes.get('/planets', PlanetController.index)
 routes.put('/planets/:id', PlanetController.put)
 routes.delete('/planets/:id', PlanetController.delete)
+
+// Routes satellites
+routes.post('/planet/:planetId/satellites', SatelliteController.store)
+routes.get('/planet/:planetId/satellites', SatelliteController.index)
 
 module.exports = routes
